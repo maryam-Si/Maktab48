cdb7c8e56ddc9e4ec62a3ec9f64b46c484086bda
maryam sadeghi <maryammaryamsadeghi6@gmail.com>
2021-05-11 15:39:07 +0430

    homework10 folfer added

--- practice5/index.js
+++
@@ -1,7 +0,0 @@
-let dropButton = document.querySelector('.dropdown-btn');
-dropButton.addEventListener('click', function () {
-    dropButton.nextElementSibling.classList.toggle('changed-dropdown-container')
-    dropButton.classList.toggle('active')
-
-
-})
